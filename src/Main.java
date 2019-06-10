6690c830667098068dfed3a6ebcfafd6c1ea4f53
dariopolv <dario.polverini93@gmail.com>
2019-06-11 01:16:12 +0200

    code reduction

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
 	public static void main(String [] args) {
 		Match m = new Match();
-		m.start();
-		while( m.getBattleground().getBool() == false) {	 
-			m.play();
-		}
+		m.runGame();
 	}
 }
